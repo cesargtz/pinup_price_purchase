d66170132e1c74b8992cabac790ea9ea2f13911a
César Gtz <cesareomargtz@gmail.com>
2017-07-06 10:19:25 -0600

    v1.1

--- pinup_price.py
+++ pinup_price.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from openerp import models, fields, api
+from openerp import models, fields, api, exceptions
 import psycopg2
 
 class pinup_price_purchase(models.Model):
@@ -12,26 +12,60 @@ class pinup_price_purchase(models.Model):
                             help="Unique number of the set prices")
 
     purchase_order_id = fields.Many2one('purchase.order')
-    partner_id = fields.Many2one('res.partner', readonly=True, related="purchase_order_id.partner_id")
+    partner_id = fields.Many2one('res.partner', readonly=True, related="purchase_order_id.partner_id", store=True)
     request_date = fields.Date(required=True, default=fields.Date.today)
     pinup_tons = fields.Float(required=True, eval="False")
     price_usd = fields.Float(compute="_compute_price", store=True)
     tc = fields.Char(compute="_compute_tc", store=True)
     tc_date = fields.Char(compute="_compute_tc_date", store=True)
     price_date = fields.Char(compute="_compute_price_date", store=True)
-    tons_reception = fields.Float(compute="_compute_tr", digits=(12,4) ,  store=True)
-    tons_invoiced = fields.Float(compute="_compute_ti", digits=(12,4) ,  store=True)
-    # tons_invoiced = fields.Many2one('account.invoice','purchase_invoice_rel','purchase_id','invoice_id',)
+    tons_reception = fields.Float(compute="_compute_tr", digits=(12,3) ,  store=True)
+    tons_invoiced = fields.Float(compute="_compute_ti", digits=(12,3) ,  store=True)
+
+    state = fields.Selection([
+        ('set', "Set"),
+        ('valued', "Valued"),
+    ], default='set')
 
 
+    _sql_constraints = [
+            ('set_price_constrain',
+            'CHECK(pinup_tons > tons_reception)',
+            "You want to bill more tons than you have delivered"),
+    ]
+
+    @api.multi
+    def action_set(self):
+        self.state = 'set'
+
+    @api.multi
+    def action_valued(self):
+        self.state = 'valued'
+
+    @api.constrains('pinup_tons','tons_reception','tons_invoiced')
+    def _check_tons(self):
+        tons_available = self.tons_reception - self.tons_invoiced
+        if self.pinup_tons > tons_available:
+            raise exceptions.ValidationError("You want to bill more tons than you have available")
 
     @api.multi
     @api.depends('purchase_order_id')
     def _compute_ti(self):
         cr = self.env.cr
-        cr.execute("""SELECT invoice_id FROM purchase_invoice_rel WHERE purchase_id = %s """ % (self.purchase_order_id.id))
-        x = cr.fetchall()
-        print(x)
+        cr.execute("""SELECT invoice_id FROM purchase_invoice_rel WHERE purchase_id = %d """ % (self.purchase_order_id.id))
+        invoice_ids = cr.fetchall()
+        tons_billing = 0
+        if invoice_ids:
+            for invoice_id in invoice_ids:
+                for only_id in invoice_id:
+                    data = self.env['account.invoice'].search([('id', '=', only_id), ('state', 'in', ['open','paid'])])
+                    for line in data:
+                        tons_billing += line['tons']
+
+            self.tons_invoiced = tons_billing
+        else:
+            self.tons_invoiced = 0
+
 
     @api.one
     @api.depends('purchase_order_id')
